5c6e34b9145c69d8ccc0d4c25b1af6447d45df1f
Or <or.ba402@gmail.com>
2020-06-02 22:04:21 +0300

    ALL operators added.
    cpu struct modification.
    opcode enum.
    imm sign extension now supports negative numbers.
    Code tested on 10 first commands of fib.asm and works.

--- sim/sim/main.c
+++ sim/sim/main.c
@@ -10,21 +10,23 @@
 #define WORDS_IN_SECTOR 128 // 512 bytes/4=128 words
 #define OPCODE_MASK 0x000000FF
 #define REGISTER_MASK 0x0000000F
-#define IMM_MASK 0x00000FFF
-				
+#define BRANCH_MASK 0x00000FFF //8=1000 to keep the sign of imm
+
+#define NUM_OF_OPCODES 19
 
 typedef enum reg {
 	$zero = 0,
 	$imm, $v0, $a0, $a1, $t0, $t1, $t2, $t3, $s0, $s1, $s2, $gp, $sp, $fp, $ra
 }reg;
 
-struct SIMP_registers {
-	int registers_values[NUM_OF_REGISTERS];
-};
+typedef enum opcode {
+	add = 0,
+	sub,and,or,sll,sra,srl,beq,bne,blt,bgt,ble,bge,jal,lw,sw,reti,in,out,halt
+}opcode;
 
 
 struct instruction {
-	int opcode;	  //TODO
+	opcode opcode;
 	reg rd;
 	reg rs;
 	reg rt;
@@ -34,17 +36,46 @@ struct instruction {
 struct CPU {
 	int PC;
 	struct instruction *inst;
-	struct SIMP_registers registers;
+	int registers[NUM_OF_REGISTERS];
 	int IORegisters[NUM_OF_IORegisters];
+	unsigned int *memory;
 };
 
 
 struct CPU * sim_init();
-void fetch_address(const unsigned int *memory, struct CPU *cpu);
+void fetch_address(struct CPU *cpu);
+int signExtension(int instruction);
 unsigned int *parseMemin(char *memory_file);
 void memoryOut(unsigned int *memory, char *out_file);
-unsigned int(*parseDisk(char *disk_file))[DISK_SECTORS];
-void diskOut(unsigned int **disk, char *out_file);
+void parseDisk(unsigned int(*disk)[WORDS_IN_SECTOR], char *disk_file);
+void diskOut(unsigned int(*disk)[WORDS_IN_SECTOR], char *out_file);
+
+void (*operation[NUM_OF_OPCODES]) (struct CPU *cpu);
+
+void add_op(struct CPU *cpu);
+void sub_op(struct CPU *cpu);
+void and_op(struct CPU *cpu);
+void or_op(struct CPU *cpu);
+void sll_op(struct CPU *cpu);
+void sra_op(struct CPU *cpu);
+void srl_op(struct CPU *cpu);
+void beq_op(struct CPU *cpu);
+void beq_op(struct CPU *cpu);
+void bne_op(struct CPU *cpu);
+void blt_op(struct CPU *cpu);
+void bgt_op(struct CPU *cpu);
+void ble_op(struct CPU *cpu);
+void bge_op(struct CPU *cpu);
+void jal_op(struct CPU *cpu);
+void lw_op(struct CPU *cpu);
+void sw_op(struct CPU *cpu);
+void reti_op(struct CPU *cpu);
+void in_op(struct CPU *cpu);
+void out_op(struct CPU *cpu);
+void halt_op(struct CPU *cpu);
+
+void initializeOperators();
+void executeInstruction(struct CPU *cpu);
 
 
 int main(int argc, char** argv)
@@ -52,18 +83,25 @@ int main(int argc, char** argv)
 	//init cpu struct
 	struct CPU *cpu =sim_init();
 	//parse input files
-	unsigned int *memory = parseMemin(argv[1]);
-	unsigned int **disk = parseDisk(argv[2]);
+	cpu->memory = parseMemin(argv[1]);
+	unsigned int(*disk)[WORDS_IN_SECTOR]= (unsigned int(*)[WORDS_IN_SECTOR])malloc(sizeof(*disk)*DISK_SECTORS);
+	int i;
+	//initiliaze each sector to 0
+	for (i = 0; i < DISK_SECTORS; i++)
+	{
+		memset(&disk[i], 0x00000000, WORDS_IN_SECTOR * sizeof(unsigned int));
+	}
+	parseDisk(disk,argv[2]);
 	//unsigned int irq2_occurences=parseIrq2(argv[3]);
 
-	fetch_address(memory,cpu);
-	while (cpu->inst->opcode != 19) //change to enum HALT
+	fetch_address(cpu);
+	while (cpu->inst->opcode != halt) //19
 	{
-		cpu->PC += 1;
-		fetch_address(memory, cpu);
+		executeInstruction(cpu);
+		fetch_address(cpu);
 	}
 
-	memoryOut(memory, argv[4]);
+	memoryOut(cpu->memory, argv[4]);
 	diskOut(disk, argv[11]);
 
 }
@@ -103,21 +141,12 @@ void memoryOut(unsigned int *memory, char *out_file)
 }
 
 
-unsigned int (*parseDisk(char *disk_file))[WORDS_IN_SECTOR]
+void parseDisk(unsigned int (*disk)[WORDS_IN_SECTOR],char *disk_file)
 {
-	unsigned int (*disk)[WORDS_IN_SECTOR];
-	disk = (unsigned int(*)[WORDS_IN_SECTOR])malloc(sizeof(*disk)*DISK_SECTORS);
-	int i;
-	//initiliaze each sector to 0
-	for (i = 0; i < DISK_SECTORS; i++)
-	{
-		memset(&disk[i], 0x00000000, WORDS_IN_SECTOR*sizeof(unsigned int));
-	}
 	FILE *disk_file_desc;
 	disk_file_desc = fopen(disk_file, "r");
 	assert(disk_file_desc != NULL);
-	i = 0;
-	int j = 0;
+	int i = 0,j=0;
 	while (fscanf(disk_file_desc, "%08X", &(disk[i][j])) != EOF)
 	{
 		j += 1;
@@ -127,10 +156,9 @@ unsigned int (*parseDisk(char *disk_file))[WORDS_IN_SECTOR]
 		}
 	}
 	fclose(disk_file_desc);
-	return disk;
 }
 
-void diskOut(unsigned int (*disk)[WORDS_IN_SECTOR], char *out_file)
+void diskOut(unsigned int(*disk)[WORDS_IN_SECTOR], char *out_file)
 {
 	FILE *disk_file_desc;
 	disk_file_desc = fopen(out_file, "w");
@@ -152,15 +180,236 @@ struct CPU * sim_init()
 	struct CPU *cpu = (struct CPU*)malloc(sizeof(struct CPU));
 	cpu->inst = (struct instruction*)malloc(sizeof(struct instruction));
 	cpu->PC = 0;
+	initializeOperators();
+	cpu->registers[$zero] = 0;
 	return cpu;
 }
 
-void fetch_address(const unsigned int *memory, struct CPU *cpu)
+void fetch_address(struct CPU *cpu)
 {
-	const unsigned int current_instruction = memory[cpu->PC];
+	unsigned int current_instruction = cpu->memory[cpu->PC];
 	cpu->inst->opcode = (current_instruction >>24)& OPCODE_MASK;
 	cpu->inst->rd= (current_instruction >>20) & REGISTER_MASK;
 	cpu->inst->rs= (current_instruction >> 16) & REGISTER_MASK;
 	cpu->inst->rt= (current_instruction >> 12) & REGISTER_MASK;
-	cpu->inst->imm= current_instruction & IMM_MASK;
+	cpu->inst->imm= signExtension(current_instruction);
+	cpu->registers[$imm] = cpu->inst->imm;
+}
+
+int signExtension(int instruction)  //from 3 bytes to 8 bytes
+{
+	int value = (0x00000FFF & instruction);
+	int mask = 0x00000800;
+	if (mask & instruction) {
+		value += 0xFFFFF000;
+	}
+	return value;
+}
+
+void add_op(struct CPU *cpu)
+{
+	if (cpu->inst->rd == $zero|| cpu->inst->rd==$imm)
+	{
+		cpu->PC += 1;
+		return;
+	}
+	cpu->registers[cpu->inst->rd] = cpu->registers[cpu->inst->rs] + cpu->registers[cpu->inst->rt];
+	cpu->PC += 1;
+}
+
+
+void sub_op(struct CPU *cpu)
+{
+	if (cpu->inst->rd == $zero || cpu->inst->rd == $imm)
+	{
+		cpu->PC += 1;
+		return;
+	}
+	cpu->registers[cpu->inst->rd] = cpu->registers[cpu->inst->rs] - cpu->registers[cpu->inst->rt];
+	cpu->PC += 1;
+}
+
+void and_op(struct CPU *cpu)
+{
+	if (cpu->inst->rd == $zero || cpu->inst->rd == $imm)
+	{
+		cpu->PC += 1;
+		return;
+	}
+	cpu->registers[cpu->inst->rd] = cpu->registers[cpu->inst->rs] & cpu->registers[cpu->inst->rt];
+	cpu->PC += 1;
+}
+
+void or_op(struct CPU *cpu)
+{
+	if (cpu->inst->rd == $zero || cpu->inst->rd == $imm)
+	{
+		cpu->PC += 1;
+		return;
+	}
+	cpu->registers[cpu->inst->rd] = cpu->registers[cpu->inst->rs] | cpu->registers[cpu->inst->rt];
+	cpu->PC += 1;
+}
+
+void sll_op(struct CPU *cpu)
+{
+	if (cpu->inst->rd == $zero || cpu->inst->rd == $imm)
+	{
+		cpu->PC += 1;
+		return;
+	}
+	cpu->registers[cpu->inst->rd] = cpu->registers[cpu->inst->rs] << cpu->registers[cpu->inst->rt];
+	cpu->PC += 1;
+}
+
+
+void sra_op(struct CPU *cpu)
+{
+	if (cpu->inst->rd == $zero || cpu->inst->rd == $imm)
+	{
+		cpu->PC += 1;
+		return;
+	}
+	//arithmetic shift with sign extension
+	if (cpu->registers[cpu->inst->rs] < 0 && cpu->registers[cpu->inst->rt] > 0)
+	{
+		cpu->registers[cpu->inst->rd] = cpu->registers[cpu->inst->rs] >> cpu->registers[cpu->inst->rt] | ~(~0U >> cpu->registers[cpu->inst->rt]);
+	}
+	else
+	{
+		cpu->registers[cpu->inst->rd] = cpu->registers[cpu->inst->rs] >> cpu->registers[cpu->inst->rt];
+
+	}
+	cpu->PC += 1;
+}
+
+void srl_op(struct CPU *cpu)
+{
+	if (cpu->inst->rd == $zero || cpu->inst->rd == $imm)
+	{
+		cpu->PC += 1;
+		return;
+	}
+	//logical shift with sign extension
+	cpu->registers[cpu->inst->rd] = (unsigned)(cpu->registers[cpu->inst->rs]) >> cpu->registers[cpu->inst->rt];
+	cpu->PC += 1;
+}
+
+void beq_op(struct CPU *cpu)
+{
+	if (cpu->registers[cpu->inst->rs] == cpu->registers[cpu->inst->rt])
+	{
+		cpu->PC = cpu->registers[cpu->inst->rd] & BRANCH_MASK;
+	}
+}
+
+void bne_op(struct CPU *cpu)
+{
+	if (cpu->registers[cpu->inst->rs] != cpu->registers[cpu->inst->rt])
+	{
+		cpu->PC = cpu->registers[cpu->inst->rd] & BRANCH_MASK;
+	}
+}
+
+void blt_op(struct CPU *cpu)
+{
+	if (cpu->registers[cpu->inst->rs] < cpu->registers[cpu->inst->rt])
+	{
+		cpu->PC = cpu->registers[cpu->inst->rd] & BRANCH_MASK;
+	}
+}
+
+void bgt_op(struct CPU *cpu)
+{
+	if (cpu->registers[cpu->inst->rs] > cpu->registers[cpu->inst->rt])
+	{
+		cpu->PC = cpu->registers[cpu->inst->rd] & BRANCH_MASK;
+	}
+}
+
+void ble_op(struct CPU *cpu)
+{
+	if (cpu->registers[cpu->inst->rs] <= cpu->registers[cpu->inst->rt])
+	{
+		cpu->PC = cpu->registers[cpu->inst->rd] & BRANCH_MASK;
+	}
+}
+
+void bge_op(struct CPU *cpu)
+{
+	if (cpu->registers[cpu->inst->rs] >= cpu->registers[cpu->inst->rt])
+	{
+		cpu->PC = cpu->registers[cpu->inst->rd] & BRANCH_MASK; //address mask
+	}
+}
+
+void jal_op(struct CPU *cpu)
+{
+	cpu->registers[$ra] = cpu->PC + 1;
+	cpu->PC = cpu->registers[cpu->inst->rd] & BRANCH_MASK;
+}
+
+void lw_op(struct CPU *cpu)
+{
+	cpu->registers[cpu->inst->rd] = cpu->memory[cpu->registers[cpu->inst->rs] + cpu->registers[cpu->inst->rt]];
+	cpu->PC += 1;
+}
+
+void sw_op(struct CPU *cpu)
+{
+	cpu->memory[cpu->registers[cpu->inst->rs] + cpu->registers[cpu->inst->rt]] = cpu->registers[cpu->inst->rd];
+	cpu->PC += 1;
+}
+
+void reti_op(struct CPU *cpu)
+{
+	cpu->PC = cpu->IORegisters[cpu->registers[cpu->inst->rs] + cpu->registers[cpu->inst->rt]];
+}
+
+
+void in_op(struct CPU *cpu)
+{
+	cpu->registers[cpu->inst->rd] = cpu->IORegisters[cpu->registers[cpu->inst->rs] + cpu->registers[cpu->inst->rt]];
+	cpu->PC += 1;
+}
+
+void out_op(struct CPU *cpu)
+{
+	cpu->IORegisters[cpu->registers[cpu->inst->rs] + cpu->registers[cpu->inst->rt]] = cpu->registers[cpu->inst->rd];
+	cpu->PC += 1;
+}
+
+void halt_op(struct CPU *cpu)
+{
+	return;
+}
+
+
+void initializeOperators()	//intializing an array of pointers to functions
+{
+	operation[add] = add_op; //0
+	operation[sub] = sub_op; //1
+	operation[and] = and_op; //2
+	operation[or] = or_op;	 //3
+	operation[sll ] = sll_op; //4
+	operation[sra] = sra_op; //5
+	operation[srl] = srl_op; //6
+	operation[beq] = beq_op; //7
+	operation[bne] = bne_op; //8
+	operation[blt] = blt_op; //9
+	operation[bgt] = bgt_op; //10
+	operation[ble] = ble_op; //11
+	operation[bge] = bge_op; //12
+	operation[jal] = jal_op; //13
+	operation[lw] = lw_op; //14
+	operation[sw] = sw_op; //15
+	operation[reti] = reti_op; //16
+	operation[in] = in_op; //17
+	operation[out] = out_op; //18
+	operation[halt] =halt_op; //19
+}
+
+void executeInstruction(struct CPU *cpu)
+{
+	(*operation[cpu->inst->opcode]) (cpu);
 }
